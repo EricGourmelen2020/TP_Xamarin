7842791b92e9d9a8a8f152cd1e9ba7d7bda4662f
Gourmelen Eric <eric.gourmelen2019@campus-eni.fr>
2020-06-24 16:32:55 +0200

    TP module 5-6

---
+++ Module06_TP01/Module06_TP01/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module06_TP01
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Connection_Clicked()
+        {
+            error.Text = "";
+            error.IsVisible = false;
+
+            if (this.id.Text == null || string.IsNullOrEmpty(this.id.Text) || this.id.Text.Length < 3)
+            {
+                error.Text = "L'identifiant doit posséder au moins 3 carractères.";
+                error.IsVisible = true;
+                return;
+            }
+            if (this.mdp.Text == null || string.IsNullOrEmpty(this.mdp.Text) || this.mdp.Text.Length < 6)
+            {
+                error.Text = "Le mot de passe doit posséder au moins 6 carractères.";
+                error.IsVisible = true;
+                return;
+            }
+            this.scroll.IsVisible = true;
+            this.form.IsVisible = false;
+
+        }
+    }
+}
